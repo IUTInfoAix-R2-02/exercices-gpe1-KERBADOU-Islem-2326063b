029cb729881d3ca303adcc2b65eddbf5e0ea8579
KERBADOU Islem <islem.kerbadou@etu.univ-amu.fr>
2024-05-16 12:07:56 +0200

    feat: exercice 7, j'ai pris 1h de retard du à un bug avec le fichier target

--- src/main/java/fr/amu/iut/exercice7/CounterMain.java
+++ src/main/java/fr/amu/iut/exercice7/CounterMain.java
@@ -3,6 +3,7 @@ package fr.amu.iut.exercice7;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
