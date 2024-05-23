18d4b233c23daffef3828ce5420802645eca3c87
KERBADOU Islem <islem.kerbadou@etu.univ-amu.fr>
2024-05-23 15:58:22 +0200

    feat: exercice 13

--- src/main/java/fr/amu/iut/exercice13/MainPersonnes.java
+++ src/main/java/fr/amu/iut/exercice13/MainPersonnes.java
@@ -1,5 +1,6 @@
-package fr.amu.iut.exercice3;
+package fr.amu.iut.exercice13;
 
+import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -11,13 +12,40 @@ public class MainPersonnes  {
 
     private static ListChangeListener<Personne> unChangementListener;
 
+    private static ListChangeListener<Personne> plusieursChangementsListener;
+
     public static void main(String[] args) {
 
-        lesPersonnes = FXCollections.observableArrayList();
+        lesPersonnes = FXCollections.observableArrayList(personne -> new Observable[] {personne.ageProperty()});;
 
-//        unChangementListener = à completer
 
-        lesPersonnes.addListener(unChangementListener);
+        plusieursChangementsListener =  new ListChangeListener<Personne>() {
+            @Override
+            public void onChanged(Change<? extends Personne> change) {
+
+                change.next();
+
+                if (change.wasRemoved()) {
+                    System.out.println(change.getRemoved().get(0).getNom());
+                }
+
+                else if (change.wasUpdated()){
+                    System.out.println(lesPersonnes.get(change.getFrom()).getNom() + " a maintenant " + lesPersonnes.get(change.getFrom()).getAge()  + " ans");
+                }
+
+                else System.out.println(change.getAddedSubList().get(0).getNom());
+
+
+            }
+        };
+
+        lesPersonnes.addListener(plusieursChangementsListener);
+
+        // question1();
+        // question2();
+        // question3();
+        question5();
+
     }
 
     public static void question1() {
@@ -28,6 +56,8 @@ public class MainPersonnes  {
         lesPersonnes.add(paul);
         lesPersonnes.add(jacques);
     }
+
+
 
     public static void question2() {
         Personne pierre = new Personne("Pierre", 20);
